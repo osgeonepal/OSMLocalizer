3bde934eeac2d80ff8a7e0bb25007fcf883db4e1
Aadesh-Baral <elkloppiko@gmail.com>
2022-12-19 16:34:53 +0545

    Initiallize database, add db migrate commands

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from backend.config import EnvironmentConfig
 
-def create_app(g):
+db = SQLAlchemy()
+
+def create_app(config=EnvironmentConfig):
     app = Flask(__name__)
+    app.config.from_object(config)
+    db.init_app(app)
     
     CORS(app)
         
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,10 @@
 from flask.cli import FlaskGroup
+from flask_migrate import Migrate
 
-from backend import create_app
+from backend import create_app, db
 
 app=create_app()
+migrate = Migrate(app, db)
 
 cli = FlaskGroup(create_app=create_app)
 
