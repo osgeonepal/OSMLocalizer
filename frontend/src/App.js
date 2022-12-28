c4e19337186dba2cf80c4aae0d0a53c1b5e87add
Aadesh-Baral <elkloppiko@gmail.com>
2022-12-28 18:28:42 +0545

    Add ui components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,73 +1,104 @@
-import React, { useEffect, useState } from "react"
-import "./App.css"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import logo from "./logo.png";
 
 function App() {
-    const [textToTranslateDetails, setTextToTranslateDetails] = useState()
-    const [text, setText] = useState()
-  
-    const fetchData = (url) => {
-      fetch(url)
-        .then(response => {
-          return response.json()
-        })
-        .then(data => {
-          setTextToTranslateDetails(data)
-        })
-    }
-    
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      const url = "http://localhost:5000/translate"
-      const data = { text }
-      fetch(url, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json"
-        }
+  const [textToTranslateDetails, setTextToTranslateDetails] = useState()
+  const [text, setText] = useState()
+
+  const fetchData = (url) => {
+    fetch(url)
+      .then(response => {
+        return response.json()
       })
-        .then(response => {
-          return response.json()
-        })
-        .then(data => {
-          setTextToTranslateDetails(data)
-        })
-    }
+      .then(data => {
+        setTextToTranslateDetails(data)
+      })
+  }
 
-    const handleInputChange = (e) => {
-      setText(e.target.value)
-    }
-    
-    const onSuggestionClick = (e) => {
-      setText(e.target.innerText)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const url = "http://localhost:5000/translate"
+    const data = { text }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setTextToTranslateDetails(data)
+      })
+  }
 
-    useEffect(() => {
-      fetchData("http://localhost:5000/translate")
-    }, [])
+  const handleInputChange = (e) => {
+    setText(e.target.value)
+  }
+
+  const onSuggestionClick = (e) => {
+    setText(e.target.innerText)
+  }
+
+  useEffect(() => {
+    fetchData("http://localhost:5000/translate")
+  }, [])
 
   return (
     <div className="App">
       {textToTranslateDetails && (
-          <header className="App-header">
-            <h3>
-              Text to translate: {textToTranslateDetails.text}
-            </h3>
-            <p>This word appeared {textToTranslateDetails.count} times in your region</p>
-            <input type="text" onChange={handleInputChange} value={text}/>
-            <div>
-              <span>Suggestion:</span>
-              <span 
-                className="suggestion"
-                onClick={onSuggestionClick}
+        <header className="App-header">
+          <div className="banner">
+            <img
+              className="banner-logo"
+              src={logo}
+              alt="logo"
+            />
+            <span
+              className="banner-name"
+            >
+              OSM LOCALIZER
+            </span>
+          </div>
+          <div className="translate-component">
+            <span className="translate-title">Translate:</span>
+            <span className="translate-text">
+              {textToTranslateDetails.text}
+            </span>
+            <span
+              className="info-icon"
+              data-bs-toggle="tooltip"
+              data-bs-html="true"
+              title={`Word count: ${textToTranslateDetails.count}. Click on the icon for more details.`}
+            >
+              <i class="fa fa-info-circle"></i>
+            </span>
+          </div>
+          <a href="https://google.com" target="_blank" className="correct-text" rel="noopener noreferrer">correct text</a>
+          <div className="translation-form">
+            <form>
+              <input
+                type="text"
+                id="user-translation"
+              />
+              <button type="submit">Submit translation</button>
+              <div className="suggestion-component">
+                <span>Suggestion:</span>
+                <span
+                  className="suggestion-text"
+                  onClick={onSuggestionClick}
                 >
                   नेपाल
-              </span>
-            </div>
-            <button onClick={handleSubmit}>Translate</button>
-            <button>Skip</button>
-          </header>
-        )}
+                </span>
+              </div>
+              <button type="button">Skip</button>
+            </form>
+          </div>
+        </header>
+      )}
     </div>
   );
 }
