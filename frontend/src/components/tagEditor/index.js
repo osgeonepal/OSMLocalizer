3b750c247735ee8d5868d3443c17a9c9f1233672
Aadesh-Baral <elkloppiko@gmail.com>
2023-02-05 11:36:01 +0545

    Complete initial editor

--- frontend/src/components/tagEditor/index.js
+++ frontend/src/components/tagEditor/index.js
@@ -3,14 +3,19 @@ import React, { useState, useEffect } from 'react';
 import { fetchExternalJSONAPI } from "../../utills/fetch";
 import Map from './map';
 import { TagEditorForm } from './editForm';
+import { getUserDetails } from '../../utills/osm';
+import { uploadToOSM } from '../../utills/osm';
+import { DEFAULT_CHANGESET_COMMENT } from '../../config';
 
-var id = 5067983922;
+var id = 9928590059;
 var type = "node";
-const count = 0
 
 export default function TagEditor() {
     const [element, setElement] = useState([]);
+    const [allChanges, setAllChanges] = useState({});
+    const [user, setUser] = useState();
     const [isLoading, setLoading] = useState(true);
+    const [isUploading, setUploading] = useState(false);
     useEffect(() => {
         (async () => {
             setLoading(true);
@@ -19,30 +24,80 @@ export default function TagEditor() {
             setLoading(false);
         })();
     }, []);
+
+
+    useEffect(() => {
+        getUserDetails()
+            .then((response) => JSON.parse(response))
+            .then((data) => {
+                setUser(data["user"]);
+            });
+
+    }, []);
+
+    const onUpload = () => {
+        setUploading(true)
+        uploadToOSM(Object.values(allChanges), DEFAULT_CHANGESET_COMMENT)
+            .then(
+                setUploading(false),
+                setAllChanges({})
+            )
+    }
+
     return (
         <div className='h-auto border border-secondary-subtle p-2 pt-0'>
             {isLoading ? <div>Loading...</div> :
                 (<div>
                     <div className='row p-2'>
-                        <div className="col-6 d-flex justify-content-start fs-6 text-secondary align-items-center">
-                            <span>{element.type}: </span> <span>{element.id}</span>
+                        <div className="col-5 d-flex justify-content-start border border-secondary-subtle p-1">
+                            {user ? (
+                                <div className='row '>
+                                    <div className="col-4">
+                                        <img
+                                            src={user["img"]["href"]}
+                                            alt={user["display_name"]}
+                                            className="img-fluid col-8"
+                                        />
+                                    </div>
+                                    <div className='col-7 d-flex align-items-center text-secondary'>
+                                        {user["display_name"]}
+                                        {/* <p>Changesets: {user["changesets"]["count"]}</p> */}
+                                    </div>
+                                </div>
+                            ) : null}
                         </div>
-                        <div className='col-6 d-flex justify-content-end align-items-center'>
-                            <div>
+                        <div className='col-7 d-flex justify-content-end align-items-center'>
+                            {/* <div>
                                 <button className="btn btn-secondary">
                                     <i className="fa fa-undo " aria-hidden="true"></i>
                                 </button>
                                 <button className="btn btn-secondary">
                                     <i className="fa fa-repeat" aria-hidden="true"></i>
                                 </button>
-                            </div>
-                            <button className="btn btn-secondary ms-2" type="submit">
-                                <i className="fa fa-upload"> {count}</i>
+                            </div> */}
+                            <button
+                                className="btn btn-secondary ms-2"
+                                type="submit"
+                                disabled={(isUploading || Object.keys(allChanges).length === 0) ? true : false}
+                                onClick={onUpload}
+                            >
+                                {isUploading ? <i className="fa fa-spinner fa-spin"></i> : (
+                                    <div>
+                                        <i className="fa fa-upload"></i>
+                                        <span className='badge'>{Object.keys(allChanges).length}</span>
+                                    </div>
+                                )}
+
                             </button>
                         </div>
                     </div>
                     <Map element={element} />
-                    <TagEditorForm element={element} setElement={setElement} />
+                    <TagEditorForm
+                        element={element}
+                        allChanges={allChanges}
+                        setElement={setElement}
+                        setAllChanges={setAllChanges}
+                    />
                 </div>)
             }
         </div>
--- frontend/src/components/tagEditor/inputToolForm.js
+++ frontend/src/components/tagEditor/inputToolForm.js
@@ -1,25 +1,64 @@
-
 import React, { useState } from 'react';
 import { inputTools } from "../../utills/translator";
 
 export default function InputToolForm() {
     const [transliterate, setTransliterate] = useState([]);
+    const [isExpanded, setExpanded] = useState(false);
+    const [isCopied, setCopied] = useState(false);
     async function getInputToolSuggestions(text) {
         await inputTools(text, "ne").then((data) => setTransliterate(data));
 
     }
+    const handleExpand = () => {
+        setExpanded(!isExpanded);
+    }
+
+    const alertComponent = () => {
+        return (
+            <div className="alert alert-success position-fixed bottom-0 end-0 p-2 m-2">
+                Text copied to clipboard
+            </div>
+        )
+    }
+
+    const handleCopy = (e) => {
+        navigator.clipboard.writeText(e.target.innerText);
+        setCopied(true);
+        setTimeout(() => {
+            setCopied(false);
+        }
+            , 1200);
+
+    }
+
     return (
         <div>
-            <a className="text-decoration-none fw-bold" data-bs-toggle="collapse" aria-controls="collapseExample" aria-expanded="true" href="#collapseExample" role="button"> &gt; Input tool</a>
-            <div className="collapse p-2 ps-3" id="collapseExample">
-                <input className="form-control form-control-sm" onChange={(e) => getInputToolSuggestions(e.target.value)}></input>
+            <p
+                className="text-decoration-none fw-bold"
+                onClick={handleExpand}
+                role="button"
+            >
+                {isExpanded ? "▼" : "►"} Input tool
+            </p>
+            <div className={isExpanded ? "p-2 ps-3" : "d-none"}>
+                <input
+                    className="form-control form-control-sm"
+                    onChange={(e) => getInputToolSuggestions(e.target.value)}
+                />
                 <div className="d-flex flex-wrap justify-content-center">
                     {transliterate.map((item, index) => (
-                        <span className=" text-bg-dark m-1 p-1 rounded" key={index}>{item}</span>
+                        <span
+                            style={{ cursor: "pointer" }}
+                            onClick={(e) => handleCopy(e)}
+                            className=" text-bg-dark m-1 p-1 rounded"
+                            key={index}
+                        >
+                            {item}
+                        </span>
                     ))}
+                    {isCopied ? alertComponent() : null}
                 </div>
             </div>
         </div>
-
     )
 }
