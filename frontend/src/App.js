3e268bc400f1c7bc11b7f02e872928de8c0b71b4
Aadesh-Baral <elkloppiko@gmail.com>
2023-02-05 11:36:01 +0545

    Complete initial editor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { Router } from "@reach/router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 
 import "./App.css";
 import Header from "./components/header";
 import ChallengeDetailView from "./views/challengeDetail";
+import ChallengesView from "./views/challenges";
+import CreateChallengeView from "./views/createChallenge";
 
 function App() {
 
   return (
     <div>
-      <Router>
-        <Header path="/*" />
-      </Router>
       <Container>
-        <Router>
-            <ChallengeDetailView path="/" />
-        </Router>
+        < BrowserRouter >
+        <Header path="/*" />
+        <Routes>
+            <Route path="/" element={<ChallengeDetailView />} />
+            <Route path="/challenges" element={<ChallengesView />} />
+            <Route path="/create" element={<CreateChallengeView />} />
+          </Routes>
+        </BrowserRouter>
       </Container>
     </div>
    
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,36 +1,39 @@
-import { Link } from '@reach/router';
+import { Link, Outlet } from 'react-router-dom';
 
 const menuItems = [
     {
       label: 'challenges',
-      link: '/challenges',
+      link: 'challenges',
     },
     {
       label: 'create',
-      link: '/create',
+      link: 'create'
     }
   ];
 
 
 function BasicExample() {
   return (
-    <nav className='navbar navbar-expand-lg bg-body-tertiary'>
-      <div className='container'>
-            <Link className="navbar-brand" to='/' key='/'>
-                    OSM Localizer
-            </Link>
-        <div className='navbar-toggle' aria-controls="basic-navbar-nav" />
-        <div className='navbar-collapse' id="basic-navbar-nav">
-          <div className="nav me-auto">
-            {menuItems.map((item) => (
-                <Link className="nav-link" to={item.link} key={item.link}>
-                    {item.label}
-                </Link>
-            ))}
+    <>
+      <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+        <div className='container'>
+              <Link className="navbar-brand" to='/' key='/'>
+                      OSM Localizer
+              </Link>
+          <div className='navbar-toggle' aria-controls="basic-navbar-nav" />
+          <div className='navbar-collapse' id="basic-navbar-nav">
+            <div className="nav me-auto">
+              {menuItems.map((item) => (
+                  <Link className="nav-link" to={"/"+item.link} key={item.link}>
+                      {item.label}
+                  </Link>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
