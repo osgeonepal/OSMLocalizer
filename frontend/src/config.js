22c65d6740c56251aab8513d784d59d5edfbec4b
Aadesh-Baral <elkloppiko@gmail.com>
2023-03-09 11:42:39 +0545

    Add config for environment variables

---
+++ frontend/src/config.js
@@ -0,0 +1,5 @@
+export const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+export const OAUTH_CLIENT_ID = process.env.REACT_APP_OAUTH_CLIENT_ID;
+export const OAUTH_CLIENT_SECRET = process.env.REACT_APP_OAUTH_CLIENT_SECRET;
+export const DEFAULT_CHANGESET_COMMENT = 
+  process.env.REACT_APP_DEFAULT_CHANGESET_COMMENT || "#OSMLocalizer #LocalizeNepal";
