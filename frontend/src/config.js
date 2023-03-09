e8d0fc486443d64d970873d25194c437c51273dd
Aadesh-Baral <elkloppiko@gmail.com>
2023-03-09 14:53:03 +0545

    Pass API_URL form env to config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,3 +3,5 @@ export const OAUTH_CLIENT_ID = process.env.REACT_APP_OAUTH_CLIENT_ID;
 export const OAUTH_CLIENT_SECRET = process.env.REACT_APP_OAUTH_CLIENT_SECRET;
 export const DEFAULT_CHANGESET_COMMENT = 
   process.env.REACT_APP_DEFAULT_CHANGESET_COMMENT || "#OSMLocalizer #LocalizeNepal";
+
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:5000";
