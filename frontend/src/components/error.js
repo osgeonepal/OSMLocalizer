9e49881a1540a1368d7d041f9fc1c55587e20cff
Aadesh-Baral <elkloppiko@gmail.com>
2023-03-11 13:48:11 +0545

    Add model to display error

---
+++ frontend/src/components/error.js
@@ -0,0 +1,55 @@
+import { useNavigate } from "react-router-dom";
+
+const errorMessages = {
+  NO_FEATURES_TO_LOCALIZE: {
+    title: "No features to localize",
+    message:
+      "Congratulations! There are no more features left to localize in this challenge. You can either validate features or check out other challenges.",
+  },
+  OTHER: {
+    title: "Something went wrong",
+    message: "We are sorry, something went wrong. Please try again later.",
+  },
+};
+
+const ShowError = ({ error, setError }) => {
+  const navigate = useNavigate();
+  Object.keys(errorMessages).includes(error) || (error = "OTHER");
+  const { title, message } = errorMessages[error];
+  return (
+    <div>
+      <div
+        className="modal fade show"
+        style={{ display: "block", backgroundColor: "transparent" }}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5">{title}</h1>
+            </div>
+            <div className="modal-body">
+              <p>{message}</p>
+            </div>
+            <div className="modal-footer">
+              <button
+                className="btn btn-secondary"
+                onClick={() => setError(null)}
+              >
+                Close
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => navigate("/challenges")}
+              >
+                Go to Challenges
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="modal-backdrop fade show"></div>
+    </div>
+  );
+};
+
+export default ShowError;
