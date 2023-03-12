49cd1e012f6ccc2bc13d889589860443d8b5e2f6
Aadesh-Baral <elkloppiko@gmail.com>
2023-03-13 00:27:54 +0545

    Replace gevent with tornado

--- backend/gunicorn.py
+++ backend/gunicorn.py
@@ -1,7 +1,7 @@
 import os
 
 bind = "0.0.0.0:5000"
-worker_class = "gevent"
+worker_class = "tornado"
 workers = (os.cpu_count() or 1) * 2 + 1
 threads = (os.cpu_count() or 1) * 2 + 1
 preload = True
