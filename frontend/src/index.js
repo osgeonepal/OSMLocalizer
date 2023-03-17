14c5f4f11bf81316a9f09e653daf61fc5f8542d3
Aadesh-Baral <elkloppiko@gmail.com>
2023-03-17 23:58:38 +0545

    Initiallize sentry on frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 
 import "./index.css";
 import App from "./App";
 import store from "./store/store";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { SENTRY_FRONTEND_DSN, SENTRY_ENVIRONMENT } from "./config";
+
+if (SENTRY_FRONTEND_DSN) {
+  Sentry.init({
+    dsn: SENTRY_FRONTEND_DSN,
+    environment: SENTRY_ENVIRONMENT,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 0.1,
+  });
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
