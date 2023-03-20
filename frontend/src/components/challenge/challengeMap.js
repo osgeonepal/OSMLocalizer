78314a2aa6814af54e5a7c5b39e86fbeb1dfa84c
Aadesh-Baral <elkloppiko@gmail.com>
2023-03-20 19:47:33 +0545

    Add osm mapstyle on localize view

--- frontend/src/components/challenge/challengeMap.js
+++ frontend/src/components/challenge/challengeMap.js
@@ -21,6 +21,26 @@ export const ChallengeMap = (props) => {
     );
   }, [props.challenge.id]);
 
+  // const osm_style = {
+//     version: 8,
+//     sources: {
+//         osm: {
+//             type: 'raster',
+//             tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+//             tileSize: 256,
+//             attribution: ('Map tiles by <a target="_top" rel="noopener" href="https://tile.openstreetmap.org/">OpenStreetMap tile servers</a>,' +
+//                 'under the <a target="_top" rel="noopener" href="https://operations.osmfoundation.org/policies/tiles/">tile usage policy</a>.' +
+//                 'Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>')
+//         }
+//     },
+//     layers: [{
+//         id: 'osm',
+//         type: 'raster',
+//         source: 'osm',
+//     }],
+// }
+
+
   const mapContainerRef = useRef(null);
   mapboxGl.accessToken = MAPBOX_ACCESS_TOKEN;
   useEffect(() => {
