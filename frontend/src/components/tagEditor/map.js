f12809e596f5f118fe199419fe7f02ad7bc30112
Aadesh-Baral <elkloppiko@gmail.com>
2023-03-22 21:22:00 +0545

    Add total_contributors for challenges in challenge all api

--- frontend/src/components/tagEditor/map.js
+++ frontend/src/components/tagEditor/map.js
@@ -5,23 +5,26 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import { MAPBOX_ACCESS_TOKEN } from "../../config";
 
 const osm_style = {
-    version: 8,
-    sources: {
-        osm: {
-            type: 'raster',
-            tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
-            tileSize: 256,
-            attribution: ('Map tiles by <a target="_top" rel="noopener" href="https://tile.openstreetmap.org/">OpenStreetMap tile servers</a>,' +
-                'under the <a target="_top" rel="noopener" href="https://operations.osmfoundation.org/policies/tiles/">tile usage policy</a>.' +
-                'Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>')
-        }
+  version: 8,
+  sources: {
+    osm: {
+      type: "raster",
+      tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+      tileSize: 256,
+      attribution:
+        'Map tiles by <a target="_top" rel="noopener" href="https://tile.openstreetmap.org/">OpenStreetMap tile servers</a>,' +
+        'under the <a target="_top" rel="noopener" href="https://operations.osmfoundation.org/policies/tiles/">tile usage policy</a>.' +
+        'Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>',
     },
-    layers: [{
-        id: 'osm',
-        type: 'raster',
-        source: 'osm',
-    }],
-}
+  },
+  layers: [
+    {
+      id: "osm",
+      type: "raster",
+      source: "osm",
+    },
+  ],
+};
 
 const Map = (props) => {
   const mapContainerRef = useRef(null);
