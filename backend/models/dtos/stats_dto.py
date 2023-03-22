91c182e455b1f697c88c33c9c4d54c13c9260b0e
Aadesh-Baral <elkloppiko@gmail.com>
2023-03-22 20:39:19 +0545

    Simplify endpoint to return user stats

--- backend/models/dtos/stats_dto.py
+++ backend/models/dtos/stats_dto.py
@@ -1,12 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class UserStatsDTO(BaseModel):
     username: str
-    total_challenges: int
+    total_challenges: Optional[int]
     total_localized: int
-    total_validated: int
     total_skipped: int
-    total_already_localized: int
-    total_too_hard: int
-    total_invalid_data: int
