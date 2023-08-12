1c0fd955d8cd8302d739f4ae9c92cca8ee1c2cd3
Aadesh-Baral <elkloppiko@gmail.com>
2023-08-12 16:20:08 +0545

    Simplify Localize View page moving instructions component to editor

--- frontend/src/views/localizeChallenge.js
+++ frontend/src/views/localizeChallenge.js
@@ -3,15 +3,14 @@ import { useParams } from "react-router-dom";
 
 import ChallengeEditor from "../components/tagEditor";
 import { fetchLocalJSONAPI } from "../utills/fetch";
-import { ChallengeInstructions } from "../components/challengeInstructions";
 import ShowError from "../components/error";
 
 export const LocalizeChallengeView = ({ validationMode }) => {
   const [challenge, setChallenge] = useState({});
   const [isChallenegeLoaded, setIsChallengeLoaded] = useState(false);
-  const [nearbyTask, setNearbyTask] = useState(true);
   const [error, setError] = useState(null);
   const { id } = useParams();
+
   useEffect(() => {
     fetchLocalJSONAPI(`challenge/${id}/`)
       .then((data) => {
@@ -22,28 +21,14 @@ export const LocalizeChallengeView = ({ validationMode }) => {
         setError(error.message);
       });
   }, [id]);
+
   return (
     <div>
       {isChallenegeLoaded ? (
-        <div className="row">
-          <div className="col-4 p-0">
-            <ChallengeInstructions
-              challenge={challenge}
-              nearbyTask={nearbyTask}
-              setNearbyTask={setNearbyTask}
-            />
-          </div>
-          <div className="col-8">
-            <ChallengeEditor
-              challenge_id={challenge.id}
-              challengeTags={challenge.language_tags}
-              translateEngine={challenge.translate_engine}
-              nearbyTask={nearbyTask}
-              validationMode={validationMode}
-              translate_to={challenge.to_language}
-            />
-          </div>
-        </div>
+        <ChallengeEditor
+          challenge={challenge}
+          validationMode={validationMode}
+        />
       ) : (
         <div>Loading...</div>
       )}
