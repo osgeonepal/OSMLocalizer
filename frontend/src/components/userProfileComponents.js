667d0468b07e16f9a09b61ede7e87c4b47b58467
Aadesh-Baral <elkloppiko@gmail.com>
2025-01-24 17:45:25 +0545

    Add components to display top contributed challenges and contribution summary radar

--- frontend/src/components/userProfileComponents.js
+++ frontend/src/components/userProfileComponents.js
@@ -1,6 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
+import { Radar } from "react-chartjs-2";
+import "chart.js/auto";
+
+export const TopContributedProjects = ({ userChallengeData }) => {
+  return (
+    <div className="border border-0 border-end flex-grow-1">
+      <h5 className="card-title mt-4 mb-4">Top Challenges Contributed</h5>
+      <div>
+        {userChallengeData ? (
+          <ul className="list-group list-group-flush">
+            {Object.keys(userChallengeData)
+              .slice(0, 5)
+              .map((challenge) => (
+                <li
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                  key={challenge}
+                >
+                  {challenge}
+                  <span className="badge bg-primary rounded-pill">
+                    {userChallengeData[challenge]} Contributions
+                  </span>
+                </li>
+              ))}
+          </ul>
+        ) : (
+          <p>No recent contributions</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export const ContributionRadarChart = ({ userStats }) => {
+  let contributions = [
+    userStats?.total_skipped,
+    userStats?.total_validated_by_me + userStats?.total_invalidated_by_me,
+    userStats?.total_localized,
+  ];
+  // Calculate percentage of contributions
+  const totalContributions = contributions.reduce((a, b) => a + b, 0);
+  contributions = contributions.map((value) =>
+    ((value / totalContributions) * 100).toFixed(2)
+  );
+  const data = {
+    labels: ["Marked Invalid/Skipped", "Validated", "Localized"],
+    datasets: [
+      {
+        label: "Contributions",
+        data: contributions,
+        backgroundColor: "rgba(168,225,178, 0.7)",
+        borderColor: "rgba(57,211,84, 1)",
+        borderWidth: 0.1,
+        pointBackgroundColor: "#fff",
+        pointBorderColor: "rgb(33,109,57)",
+        pointBorderWidth: 2,
+      },
+    ],
+  };
+
+  // Options for the radar chart
+  const options = {
+    scales: {
+      r: {
+        beginAtZero: true,
+        grid: {
+          display: true,
+        },
+        angleLines: {
+          display: true,
+          color: "rgb(33,109,57)",
+          lineWidth: 2,
+        },
+        ticks: {
+          display: false,
+        },
+        pointLabels: {
+          callback: function (label, index) {
+            // Display value and label on separate lines
+            const value = data.datasets[0].data[index];
+            return [`${value}%`, label];
+          },
+          font: {
+            size: 11,
+          },
+          align: "end",
+        },
+        title: {
+          display: false,
+        },
+      },
+    },
+
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
+  return (
+    <div className="ps-2 pe-2">
+      <h5 className="mt-4 mb-4 card-title">Contribution Summary</h5>
+    <div style={{ width: "350px", height: "350px", margin: "0 auto" }}>
+      <Radar data={data} options={options} />
+      </div>
+    </div>
+  );
+};
 
 export const UserInfoSection = ({
   userInfo,
@@ -184,4 +292,12 @@ UserStatsCard.propTypes = {
 
 UserStatsSection.propTypes = {
   osmStats: PropTypes.object,
+};
+
+TopContributedProjects.propTypes = {
+  userChallengeData: PropTypes.object,
+};
+
+ContributionRadarChart.propTypes = {
+  userStats: PropTypes.object,
 };
