15be95dec35f3f43f10459179e7904aa165378a6
Aadesh-Baral <elkloppiko@gmail.com>
2025-01-24 17:06:51 +0545

    Add component to display user contribution stats.

--- frontend/src/components/userProfileComponents.js
+++ frontend/src/components/userProfileComponents.js
@@ -88,6 +88,77 @@ const UserEditProfile = ({ userInfo }) => {
 };
 
 
+const UserStatsCard = ({ type, localized, validated, invalidated }) => {
+  const statTypesIcons = {
+    node: "node-plus",
+    way: "diagram-2",
+    relation: "bounding-box",
+  };
+  const icon = statTypesIcons[type];
+  return (
+    <div className="card col-3">
+      <div className="card-body d-flex justify-content-between">
+        <div className="justify-content-center align-items-center d-flex">
+          <i className={`fs-2 text-primary bi bi-${icon}`} />
+        </div>
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <div>
+            <span className="fw-bold" style={{ fontSize: "12px" }}>
+              {localized}
+            </span>
+            <span style={{ fontSize: "12px" }}> {type}s localized</span>
+          </div>
+
+          <hr className="mb-0" style={{ width: "100%" }} />
+          <div className="d-flex justify-content-around">
+            <div className="d-flex flex-column align-items-center">
+              <span style={{ fontSize: "12px" }}>{validated}</span>
+              <span style={{ fontSize: "12px" }}> Valid</span>
+            </div>
+            <div className="ms-2 me-2 border-end border-dark-subtle" />
+            <div className="d-flex flex-column align-items-center">
+              <span style={{ fontSize: "12px" }}>{invalidated}</span>
+              <span style={{ fontSize: "12px" }}> Invalid</span>
+            </div>
+            <div className="ms-2 me-2 border-end border-dark-subtle" />
+            <div className="d-flex flex-column align-items-center">
+              <span style={{ fontSize: "12px" }}>
+                {localized - validated - invalidated}
+              </span>
+              <span style={{ fontSize: "12px" }}> Unchecked</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export const UserStatsSection = ({ osmStats }) => {
+  const types = ["node", "way", "relation"];
+
+  return (
+    <div>
+      <h5 className="card-title">Contributions</h5>
+      <div className="p-4 card shadow-none border-0 mb-4">
+        {/* <h5 className="card-title p-2 ms-2">Contributions</h5> */}
+        <div className="d-flex flex-wrap justify-content-between">
+          {types.map((type) => (
+            <UserStatsCard
+              key={type}
+              type={type}
+              localized={osmStats[type]?.localized}
+              validated={osmStats[type]?.validated}
+              invalidated={osmStats[type]?.invalidated}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 UserInfoSection.propTypes = {
   userInfo: PropTypes.shape({
     picture_url: PropTypes.string,
@@ -102,4 +173,15 @@ UserEditProfile.propTypes = {
   userInfo: PropTypes.shape({
     email: PropTypes.string,
   }),
+};
+
+UserStatsCard.propTypes = {
+  type: PropTypes.string,
+  localized: PropTypes.number,
+  validated: PropTypes.number,
+  invalidated: PropTypes.number,
+};
+
+UserStatsSection.propTypes = {
+  osmStats: PropTypes.object,
 };
